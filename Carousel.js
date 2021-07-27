f71a4125fc9e56cc888196531bb96a0c660d131a
vslotema <vslotema@gmail.com>
2021-07-27 19:53:49 +0200

    carousel faster react

--- Carousel.js
+++ Carousel.js
@@ -14,47 +14,40 @@ class Carousel {
   swipeStart(positionX) {
     this.start = positionX;
     this.started = true;
+    this.track.style.transition = `none`;
   }
 
   swipeEnd(positionX) {
     this.moving = false;
     this.started = false;
     this.end = positionX;
+    this.prevX = this.slideX;
 
     if (this.start > this.end) return this.goLeft();
     if (this.start < this.end) return this.goRight();
-    else {
-      this.slideX = this.prevX;
-      this.slideCards();
-    }
   }
 
-  /* SLides part of the carousel for smoother user experience */
-  slideHalf(e, positionX) {
+  slider(e, positionX) {
     const currentX = positionX;
     const diff = currentX - this.start;
     const disable =
       this.carouselContainer.offsetWidth >= this.track.offsetWidth;
-    if (
-      !disable &&
-      this.started &&
-      (diff > this.threshold || diff < -this.threshold)
-    ) {
+    if (!disable) {
       this.moving = true;
     }
 
     if (this.moving) {
-      const max = this.track.offsetWidth - this.items[0].offsetWidth;
-      const slideW =
-        this.start < currentX
-          ? this.items[0].offsetWidth
-          : -this.items[0].offsetWidth;
+      const multiplier =
+        this.carouselContainer.offsetWidth / this.items[0].offsetWidth;
 
-      const move = this.prevX + slideW / 2;
+      const max =
+        this.track.offsetWidth - this.items[0].offsetWidth * multiplier;
 
-      if (move > -max && move < 0) {
+      const move = this.prevX + diff;
+
+      if (move >= -max && move < 0) {
         this.slideX = move;
-        this.slideCards();
+        return this.slideCards();
       }
     }
   }
@@ -64,44 +57,20 @@ class Carousel {
   }
 
   goLeft() {
-    const max = this.track.offsetWidth;
-    const multiplier = Math.floor(
-      this.carouselContainer.offsetWidth / this.items[0].offsetWidth
-    );
-    const step = this.items[0].offsetWidth * multiplier;
-    let move = this.prevX - step;
-    const nextMove = move - step;
-
-    if (move > -max) {
-      if (nextMove < -max || nextMove === -max) {
-        move = -max + this.carouselContainer.offsetWidth;
-      }
-
-      this.slideX = move;
-      this.prevX = this.slideX;
-      this.slideCards();
-    }
+    const mult = Math.ceil(this.slideX / -this.items[0].offsetWidth);
+    this.slideX = -this.items[0].offsetWidth * mult;
+    this.prevX = this.slideX;
+    this.track.style.transition = `all 0.3s ease-in`;
+    this.slideCards();
   }
 
   goRight() {
-    const multiplier = Math.floor(
-      this.carouselContainer.offsetWidth / this.items[0].offsetWidth
-    );
-    let move = this.prevX + this.items[0].offsetWidth * multiplier;
-    const nextMove = move * multiplier * this.items[0].offsetWidth;
-
-    if (
-      move > this.items[0].offsetWidth ||
-      nextMove > this.items[0].offsetWidth
-    ) {
-      move = 0;
-    }
-
-    if (move < this.items[0].offsetWidth) {
-      this.slideX = move;
-      this.prevX = this.slideX;
-      this.slideCards();
-    }
+    const mult = Math.floor(this.slideX / -this.items[0].offsetWidth);
+    console.log("mult ", mult);
+    this.slideX = -this.items[0].offsetWidth * mult;
+    this.prevX = this.slideX;
+    this.track.style.transition = `all 0.3s ease-in`;
+    this.slideCards();
   }
 }
 
@@ -119,6 +88,7 @@ carousel.carouselContainer.addEventListener(
 carousel.carouselContainer.addEventListener(
   "touchend",
   (e) => {
+    console.log("e end ", e);
     document.querySelector("body").style.overflow = "auto";
     carousel.swipeEnd(e.changedTouches[0].pageX);
   },
@@ -128,7 +98,7 @@ carousel.carouselContainer.addEventListener(
 carousel.carouselContainer.addEventListener(
   "touchmove",
   (e) => {
-    carousel.slideHalf(e, e.touches[0].pageX);
+    carousel.slider(e, e.touches[0].pageX);
   },
   { passive: true }
 );
@@ -154,8 +124,7 @@ carousel.carouselContainer.addEventListener(
 carousel.carouselContainer.addEventListener(
   "mousemove",
   (e) => {
-    carousel.slideHalf(e, e.pageX);
+    carousel.slider(e, e.pageX);
   },
   { passive: true }
 );
-
